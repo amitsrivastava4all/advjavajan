3b42cb0eb757c04ac2e88f0f0f3726a874109cb4
Amit Srivastava <amit.srivastava4all@gmail.com>
2020-02-22 22:24:14 +0530

    Code

---
+++ shopping copy 6/src/com/brainmentors/shop/dao/ConnectionUtils.java
@@ -0,0 +1,44 @@
+package com.brainmentors.shop.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public interface ConnectionUtils {
+	static Connection getConnection() throws ClassNotFoundException, SQLException{
+		ResourceBundle rb = ResourceBundle.getBundle("db");
+		// com.mysql.jdbc.Driver
+		//oracle.jdbc.driver.OracleDriver
+		//Class.forName(rb.getString("drivername"));
+		Connection con = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource)ctx.lookup(rb.getString("jndiname"));
+			con= ds.getConnection();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Driver Loaded");
+		
+		//con = DriverManager.getConnection(
+//				rb.getString("dburl"), 
+//				rb.getString("userid"),
+//				rb.getString("password"));
+//		if(con!=null) {
+//		System.out.println("Connection Create");
+//		con.close();
+//		}
+		return con;
+		
+	}
+//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		getConnection();
+//	}
+}
---
+++ shopping copy 6/src/com/brainmentors/shop/dto/users/UserDTO.java
@@ -0,0 +1,28 @@
+package com.brainmentors.shop.dto.users;
+
+public class UserDTO {
+	private String userid;
+	private String password;
+	private String message;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
